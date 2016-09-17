55ffa3a464a2792177a396c7e35e8a6566e8f5ac
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2016-09-17 11:52:21 +0200

    parse ledger

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "operations on a ledger cli file"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.zip "0.1.2"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
---
+++ src/scrooge/parse.clj
@@ -0,0 +1,42 @@
+(ns scrooge.parse
+  (:require [clojure.xml :refer [parse]]
+            [clojure.zip :refer [xml-zip]]
+            [clojure.data.zip.xml :refer [xml-> xml1-> text]]))
+
+
+(defn extract-posting
+  [node]
+  (let [note (xml1-> node :note text)
+        account (xml1-> node :account :name text)
+        commodity (xml1-> node :post-amount :amount :commodity :symbol text)
+        amount (xml1-> node :post-amount :amount :quantity text)]
+    {:account account
+     :note note
+     :commodity commodity
+     :amount (Double/parseDouble amount)}))
+
+
+(defn extract-transaction
+  [node]
+  (let [date-str (xml1-> node :date text)
+        note (xml1-> node :note text)
+        payee (xml1-> node :payee text)
+        postings (mapv extract-posting
+                       (xml-> node :postings :posting))]
+    {:date date-str
+     :note note
+     :payee payee
+     :postings postings}))
+
+
+(defn extract-ledger
+  [ledger-xml]
+  (mapv extract-transaction
+        (xml-> ledger-xml :transactions :transaction)))
+
+
+(defn parse-ledger
+  [input]
+  (->> (parse input)
+       (xml-zip)
+       (extract-ledger)))
