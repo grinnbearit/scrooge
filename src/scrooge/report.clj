d7a39fc4cb7a5f0dd9645231b4527556e80ed0c9
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2017-05-27 14:25:27 +0200

    portfolio allocation

--- src/scrooge/report.clj
+++ src/scrooge/report.clj
@@ -1,7 +1,8 @@
 (ns scrooge.report
   (:require [clj-time.core :as t]
             [scrooge.core :as sc]
-            [scrooge.parse :as sp]))
+            [scrooge.parse :as sp]
+            [clojure.set :as set]))
 
 
 (def EUR "€")
@@ -15,9 +16,28 @@
 
 
 (defn net-worth
+  "Returns number of units of each commodity in Assets - Liabilities
+
+  If passed a price-map and a commodity, returns total networth in
+  that commodity"
   ([ledger]
    (_net-worth (sc/balance ledger)))
   ([ledger prices commodity]
    (-> (sc/balance ledger)
        (sc/convert-accounts prices commodity)
        (_net-worth))))
+
+
+(defn portfolio
+  "Returns fractional allocation of net-worth in each commodity
+  use include/exclude to return relative allocations instead
+
+  tolerance is passed downstream to sc/fractional"
+  [ledger prices & {:keys [include exclude tolerance]
+                    :or {exclude #{} tolerance 0.001}}]
+  (let [commodities (net-worth ledger)
+        include (or include (set (keys commodities)))
+        included-keys (set/difference include exclude)]
+    (-> {:net-worth (select-keys commodities included-keys)}
+        (sc/fractional prices :tolerance tolerance)
+        (:net-worth))))
--- test/scrooge/report_test.clj
+++ test/scrooge/report_test.clj
@@ -32,3 +32,40 @@
   (sc/aggregate "converted" 0)
   => {["Assets"] {"$" 100.0}
       ["Liabilities"] {"$" -51.0}}))
+
+
+(facts
+ "portfolio"
+
+ (portfolio "ledger" "prices")
+ => {"BTC" 0.50 "$" 0.50}
+
+ (provided
+  (net-worth "ledger")
+  => {"BTC" 10.0 "$" 100.0}
+
+  (sc/fractional {:net-worth {"BTC" 10.0 "$" 100.0}} "prices" :tolerance 0.001)
+  => {:net-worth {"BTC" 0.50 "$" 0.50}})
+
+ ;; with include
+ (portfolio "ledger" "prices" :include #{"BTC"})
+ => {"BTC" 1.00}
+
+ (provided
+  (net-worth "ledger")
+  => {"BTC" 10.0 "$" 100.0}
+
+  (sc/fractional {:net-worth {"BTC" 10.0}} "prices" :tolerance 0.001)
+  => {:net-worth {"BTC" 1.0}})
+
+
+  ;; with exclude
+ (portfolio "ledger" "prices" :exclude #{"$"})
+ => {"BTC" 1.00}
+
+ (provided
+  (net-worth "ledger")
+  => {"BTC" 10.0 "$" 100.0}
+
+  (sc/fractional {:net-worth {"BTC" 10.0}} "prices" :tolerance 0.001)
+  => {:net-worth {"BTC" 1.0}}))
