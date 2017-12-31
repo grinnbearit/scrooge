9080604c42067bb335631c66fce5f7ea636b3422
Sidhant Godiwala <sidhant.godiwala@booking.com>
2017-12-31 18:44:55 +0530

    bump clojure 1.9.0

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "operations on a ledger cli file"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.zip "0.1.2"]
-                 [clj-time "0.12.0"]
+                 [clj-time "0.14.2"]
                  [swissknife "1.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.0"]]}})
