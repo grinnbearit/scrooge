c2cdfe1b57ed839d8e6eeb184129ec11af243a9d
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2019-06-25 21:26:36 +0200

    Account Statement

--- scrooge/report.py
+++ scrooge/report.py
@@ -5,7 +5,7 @@ from scrooge.constants import EUR
 from matplotlib.ticker import FuncFormatter
 
 
-def portfolio(ledger, prices, threshold=0.01, commodity=EUR):
+def plot_portfolio(ledger, prices, threshold=0.01, commodity=EUR):
     """
     returns (figure, axes) for a net worth plot
     """
@@ -28,4 +28,26 @@ def portfolio(ledger, prices, threshold=0.01, commodity=EUR):
         plt.text(idx, data.loc[com]["amount"],
                  "{:.2f}%".format(data.loc[com]["share"]*100))
 
+    return (f, ax)
+
+
+def plot_statement(statement, commodity, from_date=None, to_date=None):
+    """
+    returns (figure, axes) for a statement plot in a particular `commodity`
+    `from_date` and `to_date` are optional and filter the statement
+    """
+    f, ax = plt.subplots(1, 1)
+    f.set_size_inches(20, 12)
+    data = (statement
+            .reset_index()
+            .loc[lambda df: df["commodity"] == commodity]
+            .loc[lambda df: (from_date is None) |\
+                            (df["date"] >= from_date)]
+            .loc[lambda df: (to_date is None) |\
+                            (df["date"] < to_date)])
+    sns.barplot(data=data, x="date", y="amount", hue="account", ax=ax)
+    ax.get_yaxis().set_major_formatter(FuncFormatter(lambda x, p: format(int(x), ',')))
+    ax.set_xlabel("")
+    ax.set_ylabel(f"Amount ({commodity})")
+    ax.set_title(f"Account Statement ({commodity})")
     return (f, ax)
